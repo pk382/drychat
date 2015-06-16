08e2204394d1371e4a38ef9ce2112c0758f0ebf9
Pradyoth Kukkapalli <pkukkapalli@squarespace.com>
2015-06-16 16:30:07 -0400

    Added nodemon and browserify support.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var browserify = require('gulp-browserify');
 
 gulp.task('start', function() {
   nodemon({
     script: 'index.js',
+    tasks: ['html', 'browserify'],
     ext: 'html js less css'
   });
 });
 
-gulp.task('default', ['start']);
+gulp.task('html', function() {
+  gulp.src('frontend/index.html')
+    .pipe(gulp.dest('.public'));
+});
+
+gulp.task('browserify', function() {
+  gulp.src('frontend/client.js')
+    .pipe(browserify())
+    .pipe(gulp.dest('.public'));
+});
+
+gulp.task('default', ['html', 'browserify', 'start']);
