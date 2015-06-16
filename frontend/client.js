f73ca3b0344d1d690cecc9c7e77203973800291e
Pradyoth Kukkapalli <pkukkapalli@squarespace.com>
2015-06-16 17:00:41 -0400

    Started using react.

--- frontend/client.js
+++ frontend/client.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+var React = require('react');
+
+var ChatBox = React.createClass({
+  render: function() {
+    return (
+      <div className="ChatBox">
+        Hello, world! I am a ChatBox.
+      </div>
+    );
+  }
+});
+React.render(
+  <ChatBox/>,
+  document.getElementById('content')
+);
