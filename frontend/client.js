05d48668befc77f38a7bc0dff3711170c872e075
Lawrence <lluk@squarespace.com>
2015-06-16 23:01:02 -0400

    Chat box style

--- frontend/client.js
+++ frontend/client.js
@@ -1,8 +1,15 @@
 var React = require('react');
 var marked = require('marked');
+//var highlight = require('highlight.js');
+
+//highlight.initHighlightingOnLoad();
+
 var data = [
-  {author: 'Pete Hunt', text: 'This is one comment'},
-  {author: 'Jordan Walke', text: 'This is *another* comment'}
+  {author: 'Pete Hunt', text: 'This is one comment', color: "#F75482", timestamp: "12:22"},
+  {author: 'Lawrence Luk', text: 'lol what why', myself: true, color: "#9D93F7", timestamp: "12:22"},
+  {author: 'Jordan Walke', text: 'This is *another* comment.', color: "#F7EC76", timestamp: "12:22"},
+  {author: 'Jordan Walke', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor augue, accumsan molestie posuere eu, hendrerit ac nibh. Nunc at nisi augue. Donec ut velit vitae mauris ornare consectetur at vitae neque. Phasellus sagittis tortor lacus, pellentesque vulputate mauris aliquet non. Vestibulum eu massa id nisi malesuada aliquam vitae a tellus. Nulla pellentesque eros sit amet enim finibus, vehicula vulputate odio cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur vulputate nibh at diam eleifend tristique. Praesent tristique dui ut massa venenatis pulvinar. Vivamus ac magna sit amet urna pretium consectetur et quis augue. Proin eget maximus nisi.Nunc eu erat consequat, cursus urna a, aliquam lorem. Maecenas vel faucibus nisi. Sed fringilla tortor et pulvinar cursus. In nec gravida sem. Suspendisse eget neque convallis, ullamcorper velit sit amet, consectetur nisi. Aenean purus libero, egestas id sagittis vel, interdum sed est. Sed tempus sagittis massa vel sodales. Ut placerat purus arcu, euismod sagittis neque interdum molestie.', color: "#F7EC76", timestamp: "12:23"},
+  {author: 'Lawrence Luk', text: "Some words//int x = 15; String cool = 'cool';", myself: true, color: "#9D93F7", timestamp: "12:24"}
 ];
 
 var ChatBox = React.createClass({
@@ -21,7 +28,7 @@ var ChatList = React.createClass({
   render: function() {
     var messageNodes = this.props.data.map(function(message) {
       return (
-        <Message author={message.author}>
+        <Message msg={message}>
           {message.text}
         </Message>
       )
@@ -47,14 +54,28 @@ var ChatForm = React.createClass({
 var Message = React.createClass({
   render: function() {
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    var generatedClass=this.props.msg.myself ? "message-container myself" : "message-container";
+
+    var chunks = this.props.msg.text.split("//");
+    var code;
+    if (chunks.length > 1) {
+      var normalText = chunks[0];
+      chunks.shift();
+      code = (<div className="codeblock"><span dangerouslySetInnerHTML={{__html: marked(normalText, {sanitize: true})}} /><pre><code>{chunks.join("")}</code></pre></div>);
+    }
+    var messageBody = !code ? (<span dangerouslySetInnerHTML={{__html: rawMarkup}} />) : (code);
     return (
-      <div className="message">
-        <h2 className="messageAuthor">
-          {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
-      </div>
-    );
+        <div className={generatedClass}>
+          <div className="author-container" style={{'borderColor': this.props.msg.color}}>
+            <div className="gravatar"></div>
+            <div className="author"><strong>{this.props.msg.author}</strong></div>
+          </div>
+          <div className="message">
+            {messageBody}
+            <div className="timestamp">{this.props.msg.timestamp}</div>
+          </div>
+        </div>
+      );
   }
 });
 
