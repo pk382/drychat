ea68090b6e9ad3f91c9fcef504674a4c1275d74a
Pradyoth Kukkapalli <pkukkapalli@squarespace.com>
2015-06-16 23:01:09 -0400

    Got simple chat server working.

--- frontend/client.js
+++ frontend/client.js
@@ -1,17 +1,54 @@
 var React = require('react');
 var marked = require('marked');
-var data = [
-  {author: 'Pete Hunt', text: 'This is one comment'},
-  {author: 'Jordan Walke', text: 'This is *another* comment'}
-];
+var $ = require('jquery');
+var io = require('socket.io-client');
+var socket = io();
 
 var ChatBox = React.createClass({
+  loadMessagesFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleMessageSend: function(message) {
+    console.log('we here');
+    var messages = this.state.data;
+    var newMessages = messages.concat([message]);
+    this.setState({data: newMessages});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: message,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, data, error) {
+        console.error(this.props.url, status, error.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadMessagesFromServer();
+    setInterval(this.loadMessagesFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="ChatBox">
         <h1>Chats</h1>
-        <ChatList data={this.props.data}/>
-        <ChatForm/>
+        <ChatList data={this.state.data}/>
+        <ChatForm onMessageSend={this.handleMessageSend}/>
       </div>
     );
   }
@@ -24,7 +61,7 @@ var ChatList = React.createClass({
         <Message author={message.author}>
           {message.text}
         </Message>
-      )
+      );
     })
     return (
       <div className="chatList">
@@ -35,11 +72,25 @@ var ChatList = React.createClass({
 });
 
 var ChatForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = React.findDOMNode(this.refs.author).value.trim();
+    var text = React.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onMessageSend({author: author, text: text});
+    React.findDOMNode(this.refs.author).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+    return;
+  },
   render: function() {
     return (
-      <div className="chatForm">
-        Hello, world! I am a ChatForm.
-      </div>
+      <form className="chatForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author"/>
+        <input type="text" placeholder="Say something..." ref="text"/>
+        <input type="submit" value="Send" />
+      </form>
     );
   }
 });
@@ -59,6 +110,6 @@ var Message = React.createClass({
 });
 
 React.render(
-  <ChatBox data={data}/>,
+  <ChatBox url="sample" pollInterval={2000}/>,
   document.getElementById('content')
 );
--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var messages = []
 
 app.use(express.static('.public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-var server = app.listen(3000, function () {
-  console.log('Example app listening at port 3000');
+app.get('/sample', function(request, response) {
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify(messages));
+});
+
+app.post('/sample', function(request, response) {
+  console.log('Got message ' + request.body.toString());
+  messages.push(request.body);
+  response.setHeader('Content-Type', 'application/json');
+  response.send(messages);
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
