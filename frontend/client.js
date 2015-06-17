50d1474721da076aede341e595107f49fa0c132c
Lawrence <lluk@squarespace.com>
2015-06-17 17:36:44 -0400

    Styling, text area, code blocks and syntax highlighting

--- frontend/client.js
+++ frontend/client.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var marked = require('marked');
 var $ = require('jquery');
+var hljs = require('highlight.js')
 var io = require('socket.io-client');
 var socket = io();
 
@@ -48,7 +49,6 @@ var ChatBox = React.createClass({
   render: function() {
     return (
       <div className="ChatBox">
-        <h1>Chats</h1>
         <ChatList data={this.state.data}/>
         <ChatForm onMessageSend={this.handleMessageSend}/>
       </div>
@@ -76,22 +76,25 @@ var ChatList = React.createClass({
 var ChatForm = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
-    var author = React.findDOMNode(this.refs.author).value.trim();
+    var author = "Lebron Jamez";
     var text = React.findDOMNode(this.refs.text).value.trim();
     if (!text || !author) {
       return;
     }
     this.props.onMessageSend({author: author, text: text});
-    React.findDOMNode(this.refs.author).value = '';
+    var cc = $(".chatList");
+    cc.stop().animate({
+      scrollTop: cc[0].scrollHeight
+    }, 500);
+    //React.findDOMNode(this.refs.author).value = '';
     React.findDOMNode(this.refs.text).value = '';
     return;
   },
   render: function() {
     return (
       <form className="chatForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Your name" ref="author"/>
-        <input type="text" placeholder="Say something..." ref="text"/>
-        <input type="submit" value="Send" />
+        <textarea placeholder="Say something..." className="message-field" ref="text"/>
+        <input type="submit" value="Send" className="message-send" Send/>
       </form>
     );
   }
@@ -100,14 +103,14 @@ var ChatForm = React.createClass({
 var Message = React.createClass({
   render: function() {
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-    var generatedClass=this.props.msg.myself ? "message-container myself" : "message-container";
+    var generatedClass = this.props.msg.myself ? "message-container myself" : "message-container";
 
     var chunks = this.props.msg.text.split("//");
     var code;
     if (chunks.length > 1) {
       var normalText = chunks[0];
       chunks.shift();
-      code = (<div className="codeblock"><span dangerouslySetInnerHTML={{__html: marked(normalText, {sanitize: true})}} /><pre><code>{chunks.join("")}</code></pre></div>);
+      code = (<div><span dangerouslySetInnerHTML={{__html: marked(normalText, {sanitize: true})}} /><div className="codeblock"><pre><code>{chunks.join("")}</code></pre></div></div>);
     }
     var messageBody = !code ? (<span dangerouslySetInnerHTML={{__html: rawMarkup}} />) : (code);
     return (
@@ -122,6 +125,11 @@ var Message = React.createClass({
         </div>
       </div>
     );
+  },
+  componentDidMount: function() {
+    $('pre code').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
   }
 });
 
@@ -129,3 +137,14 @@ React.render(
   <ChatBox url="sample" pollInterval={2000}/>,
   document.getElementById('content')
 );
+
+$("textarea").keydown(function(e){
+  // Enter was pressed without shift key
+  if (e.keyCode == 13 && !e.shiftKey)
+  {
+      // fire the submit event
+      $(".message-send").click();
+      // prevent default behavior
+      e.preventDefault();
+  }
+});
